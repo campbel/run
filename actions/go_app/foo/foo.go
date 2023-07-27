df51d1f12e8487033cbb38f3115f6c499f444994
Chris Campbell <chrs.cmpbl@gmail.com>
2023-07-26 22:02:31 -0600

    do a go app

---
+++ actions/go_app/foo/foo.go
@@ -0,0 +1,7 @@
+package foo
+
+import "github.com/charmbracelet/log"
+
+func Message(message string) {
+	log.Info(message)
+}
---
+++ actions/go_app/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/campbel/run/actions/go_app/foo"
+
+func main() {
+	foo.Message("Hello, from a Go app!")
+}
